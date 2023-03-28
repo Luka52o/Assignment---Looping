d3246a6966e5e44e94c2ff0b551ea7b580209b35
Luka Silic <silicluka520@gmail.com>
2023-03-28 10:24:16 -0400

    Finished the assignment.

--- Assignment - Looping/Program.cs
+++ Assignment - Looping/Program.cs
@@ -43,6 +43,8 @@ namespace Assignment___Looping
                 RunDiceGame();
             }
         }
+
+        // /////////////////////////////////////////////////////////////////////////////////////////////////////
         public static int max, num;
         public static void RunPrompter()
         {
@@ -102,7 +104,7 @@ namespace Assignment___Looping
 
         }
 
-       // /////////////////////////////////////////////////////////////////////////////////////////////////////
+        // /////////////////////////////////////////////////////////////////////////////////////////////////////
         public static void RunPercentPassing()
         {
             bool done = false;
@@ -121,7 +123,7 @@ namespace Assignment___Looping
                 while (!Double.TryParse(Console.ReadLine(), out studentScore))
                 {
                     Console.WriteLine("Please only enter integer values for scores.");
-                    
+
                 }
                 if (studentScore != -1)
                 {
@@ -153,7 +155,7 @@ namespace Assignment___Looping
             int inputNumber, num;
             List<int> ints = new List<int>();
             Console.WriteLine("Odd Sum Selected. Please enter a number now.");
-            while(!Int32.TryParse(Console.ReadLine(), out inputNumber))
+            while (!Int32.TryParse(Console.ReadLine(), out inputNumber))
             {
                 Console.WriteLine("Please enter a valid number with no decimal places.");
             }
@@ -182,35 +184,413 @@ namespace Assignment___Looping
                 Console.WriteLine($"The sum of every odd number less than your number, and your number itself is {ints.Sum() + inputNumber}.");
             }
         }
+
+        // /////////////////////////////////////////////////////////////////////////////////////////////////////
+        public static int rangeMin;
         public static void RunRandomNumbers()
         {
             Random generator = new Random();
             List<int> RandNums = new List<int>();
-            int rangeMax, rangeMin;
+            int rangeMax;
+            bool done = false;
             Console.WriteLine("Random Numbers Selected. The program will generate 25 random numbers within a range you input.");
             Console.WriteLine("Please enter the maximum of your range:");
             while (!Int32.TryParse(Console.ReadLine(), out rangeMax))
             {
                 Console.WriteLine("Please enter a valid number with no decimal places.");
             }
-            Console.WriteLine("Now please enter the minimum of your range");
-            while (!Int32.TryParse(Console.ReadLine(), out rangeMin))
+            Console.WriteLine("Now please enter the minimum of your range:");
+            while (!done)
             {
-                Console.WriteLine("Please enter a valid number with no decimal places.");
+                while (!Int32.TryParse(Console.ReadLine(), out rangeMin))
+                {
+                    Console.WriteLine("Please enter a valid number with no decimal places:");
+                }
+                if (rangeMin > rangeMax)
+                {
+                    Console.WriteLine("Please ensure your minimum is lesser than your maximum:");
+                }
+                else
+                {
+                    done = true;
+                }
             }
+
             for (int i = 1; i <= 25; i++)
             {
-                generator.Next(rangeMin, rangeMax - 1);
-                RandNums.Add(i);
+                RandNums.Add(generator.Next(rangeMin, rangeMax));
             }
-            for (int i = 1; i <= RandNums.Count; i++)
+            RandNums.Sort();
+            Console.Write(rangeMin + " ");
+            for (int i = 0; i < RandNums.Count; i++)
             {
-
+                Console.Write(" " + (RandNums[i]) + " ");
             }
+            Console.WriteLine(" " + rangeMax);
+            Console.WriteLine("(Numbers have been automatically sorted from least - greatest, including your range caps)");
         }
+
+        // /////////////////////////////////////////////////////////////////////////////////////////////////////
+        public static double balance = 100.00;
         public static void RunDiceGame()
         {
-            
+            bool played = false;
+            string betType;
+
+            Console.WriteLine($"Welcome to the dice game. Your current balance is ${balance}, please chose a bet from the following list:");
+            Console.WriteLine("1. DOUBLES - Roll two dice, earn double your bet if the rolls are the same");
+            Console.WriteLine("2 .NOT DOUBLES - Roll two dice, earn half your bet if the rolls do not match");
+            Console.WriteLine("3. EVEN SUM - Roll two dice, earn your bet if the sum of the rolls is an even number");
+            Console.WriteLine("4. ODD SUM - Roll two dice, earn your bet if the sum of the rolls is an odd number");
+            Console.WriteLine("Type in your choice into the console:");
+            betType = Console.ReadLine();
+            while (betType.ToUpper() != "DOUBLES" && betType.ToUpper() != "NOT DOUBLES" && betType.ToUpper() != "EVEN SUM" && betType.ToUpper() != "ODD SUM" && betType.ToUpper() != "1" && betType.ToUpper() != "2" && betType.ToUpper() != "3" && betType.ToUpper() != "4")
+            {
+                Console.WriteLine("Please enter a valid bet type:");
+                betType = Console.ReadLine();
+            }
+            if (betType.ToUpper() == "DOUBLES" || betType == "1")
+            {
+                PlayDoubles();
+            }
+            else if (betType.ToUpper() == "NOT DOUBLES" || betType == "2")
+            {
+                PlayNotDoubles();
+            }
+            else if (betType.ToUpper() == "EVEN SUM" || betType == "3")
+            {
+                PlayEvenSum();
+            }
+            else if (betType.ToUpper() == "ODD SUM" || betType == "4")
+            {
+                PlayOddSum();
+            }
         }
+        public static void PlayDoubles()
+        {
+            bool usableValueGiven = false;
+            double betValue = 0;
+            string playAgain;
+
+            while (!usableValueGiven)
+            {
+                Console.WriteLine("Now enter your bet amount:");
+                Console.Write("$");
+
+                if (!double.TryParse(Console.ReadLine(), out betValue))
+                    Console.WriteLine("Please enter a valid number value for your bet:");
+
+                else if (betValue > balance)
+                    Console.WriteLine("Please enter a bet smaller than your balance");
+                else
+                {
+                    usableValueGiven = true;
+                }
+            }
+            die die1 = new die();
+            die die2 = new die();
+            die1.RollDie6();
+            die2.RollDie6();
+
+            die1.DrawRoll();
+            Console.WriteLine();
+            die2.DrawRoll();
+
+            if (die1.Roll == die2.Roll)
+            {
+                Console.WriteLine($"WINNER! PAYOUT: ${betValue * 2}");
+                balance = balance + (betValue * 2);
+                Console.WriteLine($"ACCOUNT BALANCE: ${balance}");
+
+                if (balance != 0)
+                {
+                    Console.WriteLine("Play again? Y/N");
+                    playAgain = Console.ReadLine();
+                    if (playAgain.ToUpper() == "N")
+                    {
+                        Console.WriteLine($"Thank you for playing! Your final account balance is {balance}");
+                    }
+                    else if (playAgain.ToUpper() == "Y")
+                    {
+                        RunDiceGame();
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("ACCOUNT DEPLETED. Better luck next time!");
+                    Console.ReadLine();
+                }
+
+            }
+            else
+            {
+                Console.WriteLine($"LOSER. YOU LOST {betValue}");
+                balance = balance - (betValue);
+                Console.WriteLine($"ACCOUNT BALANCE: {balance}");
+
+                if (balance != 0)
+                {
+                    Console.WriteLine("Play again? Y/N");
+                    playAgain = Console.ReadLine();
+                    if (playAgain.ToUpper() == "N")
+                    {
+                        Console.WriteLine($"Thank you for playing! Your final account balance is {balance}");
+                    }
+                    else if (playAgain.ToUpper() == "Y")
+                    {
+                        RunDiceGame();
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("ACCOUNT DEPLETED. Better luck next time!");
+                }
+            }
+        }
+        public static void PlayNotDoubles()
+        {
+            bool usableValueGiven = false;
+            double betValue = 0;
+            string playAgain;
+
+            while (!usableValueGiven)
+            {
+                Console.WriteLine("Now enter your bet amount:");
+                Console.Write("$");
+
+                if (!double.TryParse(Console.ReadLine(), out betValue))
+                    Console.WriteLine("Please enter a valid number value for your bet:");
+
+                else if (betValue > balance)
+                    Console.WriteLine("Please enter a bet smaller than your balance");
+                else
+                {
+                    usableValueGiven = true;
+                }
+            }
+            die die1 = new die();
+            die die2 = new die();
+            die1.RollDie6();
+            die2.RollDie6();
+
+            die1.DrawRoll();
+            Console.WriteLine();
+            die2.DrawRoll();
+
+            if (die1.Roll != die2.Roll)
+            {
+                Console.WriteLine($"WINNER! PAYOUT: ${betValue / 2}");
+                balance = balance + (betValue / 2);
+                Console.WriteLine($"ACCOUNT BALANCE: ${balance}");
+
+                if (balance != 0)
+                {
+                    Console.WriteLine("Play again? Y/N");
+                    playAgain = Console.ReadLine();
+                    if (playAgain.ToUpper() == "N")
+                    {
+                        Console.WriteLine($"Thank you for playing! Your final account balance is {balance}");
+                    }
+                    else if (playAgain.ToUpper() == "Y")
+                    {
+                        RunDiceGame();
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("ACCOUNT DEPLETED. Better luck next time!");
+                    Console.ReadLine();
+                }
+
+            }
+            else
+            {
+                Console.WriteLine($"LOSER. YOU LOST {betValue}");
+                balance = balance - (betValue);
+                Console.WriteLine($"ACCOUNT BALANCE: {balance}");
+
+                if (balance != 0)
+                {
+                    Console.WriteLine("Play again? Y/N");
+                    playAgain = Console.ReadLine();
+                    if (playAgain.ToUpper() == "N")
+                    {
+                        Console.WriteLine($"Thank you for playing! Your final account balance is {balance}");
+                    }
+                    else if (playAgain.ToUpper() == "Y")
+                    {
+                        RunDiceGame();
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("ACCOUNT DEPLETED. Better luck next time!");
+                }
+            }
+        }
+        public static void PlayEvenSum()
+        {
+            bool usableValueGiven = false;
+            double betValue = 0, rollSum;
+            string playAgain;
+
+            while (!usableValueGiven)
+            {
+                Console.WriteLine("Now enter your bet amount:");
+                Console.Write("$");
+
+                if (!double.TryParse(Console.ReadLine(), out betValue))
+                    Console.WriteLine("Please enter a valid number value for your bet:");
+
+                else if (betValue > balance)
+                    Console.WriteLine("Please enter a bet smaller than your balance");
+                else
+                {
+                    usableValueGiven = true;
+                }
+            }
+            die die1 = new die();
+            die die2 = new die();
+            die1.RollDie6();
+            die2.RollDie6();
+
+            die1.DrawRoll();
+            Console.WriteLine();
+            die2.DrawRoll();
+
+            rollSum = die1.Roll + die2.Roll;
+            if (rollSum % 2 == 0)
+            {
+                Console.WriteLine($"WINNER! PAYOUT: ${betValue}");
+                balance = balance + betValue;
+                Console.WriteLine($"ACCOUNT BALANCE: ${balance}");
+
+                if (balance != 0)
+                {
+                    Console.WriteLine("Play again? Y/N");
+                    playAgain = Console.ReadLine();
+                    if (playAgain.ToUpper() == "N")
+                    {
+                        Console.WriteLine($"Thank you for playing! Your final account balance is {balance}");
+                    }
+                    else if (playAgain.ToUpper() == "Y")
+                    {
+                        RunDiceGame();
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("ACCOUNT DEPLETED. Better luck next time!");
+                    Console.ReadLine();
+                }
+
+            }
+            else
+            {
+                Console.WriteLine($"LOSER. YOU LOST {betValue}");
+                balance = balance - betValue;
+                Console.WriteLine($"ACCOUNT BALANCE: {balance}");
+
+                if (balance != 0)
+                {
+                    Console.WriteLine("Play again? Y/N");
+                    playAgain = Console.ReadLine();
+                    if (playAgain.ToUpper() == "N")
+                    {
+                        Console.WriteLine($"Thank you for playing! Your final account balance is {balance}");
+                    }
+                    else if (playAgain.ToUpper() == "Y")
+                    {
+                        RunDiceGame();
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("ACCOUNT DEPLETED. Better luck next time!");
+                }
+            }
+        }
+        public static void PlayOddSum()
+        {
+            bool usableValueGiven = false;
+            double betValue = 0, rollSum;
+            string playAgain;
+
+            while (!usableValueGiven)
+            {
+                Console.WriteLine("Now enter your bet amount:");
+                Console.Write("$");
+
+                if (!double.TryParse(Console.ReadLine(), out betValue))
+                    Console.WriteLine("Please enter a valid number value for your bet:");
+
+                else if (betValue > balance)
+                    Console.WriteLine("Please enter a bet smaller than your balance");
+                else
+                {
+                    usableValueGiven = true;
+                }
+            }
+            die die1 = new die();
+            die die2 = new die();
+            die1.RollDie6();
+            die2.RollDie6();
+
+            die1.DrawRoll();
+            Console.WriteLine();
+            die2.DrawRoll();
+
+            rollSum = die1.Roll + die2.Roll;
+            if (rollSum % 2 == 0)
+            {
+                Console.WriteLine($"LOSER. YOU LOST {betValue}");
+                balance = balance - betValue;
+                Console.WriteLine($"ACCOUNT BALANCE: {balance}");
+
+                if (balance != 0)
+                {
+                    Console.WriteLine("Play again? Y/N");
+                    playAgain = Console.ReadLine();
+                    if (playAgain.ToUpper() == "N")
+                    {
+                        Console.WriteLine($"Thank you for playing! Your final account balance is {balance}");
+                    }
+                    else if (playAgain.ToUpper() == "Y")
+                    {
+                        RunDiceGame();
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("ACCOUNT DEPLETED. Better luck next time!");
+                }
+            }
+            else
+            {
+                Console.WriteLine($"WINNER! PAYOUT: ${betValue}");
+                balance = balance + betValue;
+                Console.WriteLine($"ACCOUNT BALANCE: ${balance}");
+
+                if (balance != 0)
+                {
+                    Console.WriteLine("Play again? Y/N");
+                    playAgain = Console.ReadLine();
+                    if (playAgain.ToUpper() == "N")
+                    {
+                        Console.WriteLine($"Thank you for playing! Your final account balance is {balance}");
+                    }
+                    else if (playAgain.ToUpper() == "Y")
+                    {
+                        RunDiceGame();
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("ACCOUNT DEPLETED. Better luck next time!");
+                    Console.ReadLine();
+                }
+            }
+        }
+
     }
 }
---
+++ Assignment - Looping/die.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment___Looping
+{
+    public class die
+    {
+        private Random _generator;
+        private int _roll;
+
+        public die()
+        {
+            _generator = new Random();
+            _roll = _generator.Next(1, 7);
+        }
+        public int Roll { get { return _roll; } }
+        public override string ToString()
+        {
+            return _roll.ToString();
+        }
+        public void RollDie2()
+        {
+            _generator = new Random();
+            _roll = _generator.Next(1, 3);
+        }
+        public void RollDie3()
+        {
+            _generator = new Random();
+            _roll = _generator.Next(1, 4);
+        }
+        public void RollDie4()
+        {
+            _generator = new Random();
+            _roll = _generator.Next(1, 5);
+        }
+        public void RollDie5()
+        {
+            _generator = new Random();
+            _roll = _generator.Next(1, 6);
+        }
+        public void RollDie6()
+        {
+            _generator = new Random();
+            _roll = _generator.Next(1, 7);
+        }
+        public void RollDie7()
+        {
+            _generator = new Random();
+            _roll = _generator.Next(1, 8);
+        }
+        public void RollDie8()
+        {
+            _generator = new Random();
+            _roll = _generator.Next(1, 9);
+        }
+        public void RollDie9()
+        {
+            _generator = new Random();
+            _roll = _generator.Next(1, 10);
+        }
+
+        public void DrawRoll()
+        {
+            if (_roll == 1)
+            {
+                Console.WriteLine(" _____ ");
+                Console.WriteLine("|     |");
+                Console.WriteLine("|  o  |");
+                Console.WriteLine("|     |");
+                Console.WriteLine(" ----- ");
+            }
+            else if (_roll == 2)
+            {
+                Console.WriteLine(" _____ ");
+                Console.WriteLine("| o   |");
+                Console.WriteLine("|     |");
+                Console.WriteLine("|   o |");
+                Console.WriteLine(" ----- ");
+            }
+            else if (_roll == 3)
+            {
+                Console.WriteLine(" _____ ");
+                Console.WriteLine("| o   |");
+                Console.WriteLine("|  o  |");
+                Console.WriteLine("|   o |");
+                Console.WriteLine(" ----- ");
+            }
+            else if (_roll == 4)
+            {
+                Console.WriteLine(" _____ ");
+                Console.WriteLine("| o o |");
+                Console.WriteLine("|     |");
+                Console.WriteLine("| o o |");
+                Console.WriteLine(" ----- ");
+            }
+            else if (_roll == 5)
+            {
+                Console.WriteLine(" _____ ");
+                Console.WriteLine("| o o |");
+                Console.WriteLine("|  o  |");
+                Console.WriteLine("| o o |");
+                Console.WriteLine(" ----- ");
+            }
+            else if (_roll == 6)
+            {
+                Console.WriteLine(" _____ ");
+                Console.WriteLine("| o o |");
+                Console.WriteLine("| o o |");
+                Console.WriteLine("| o o |");
+                Console.WriteLine(" ----- ");
+            }
+            else if (_roll == 7)
+            {
+                Console.WriteLine(" _____ ");
+                Console.WriteLine("| o o |");
+                Console.WriteLine("|o o o|");
+                Console.WriteLine("| o o |");
+                Console.WriteLine(" ----- ");
+            }
+            else if (_roll == 8)
+            {
+                Console.WriteLine(" _____ ");
+                Console.WriteLine("|o o o|");
+                Console.WriteLine("| o o |");
+                Console.WriteLine("|o o o|");
+                Console.WriteLine(" ----- ");
+            }
+            else if (_roll == 9)
+            {
+                Console.WriteLine(" _____ ");
+                Console.WriteLine("|o o o|");
+                Console.WriteLine("|o o o|");
+                Console.WriteLine("|o o o|");
+                Console.WriteLine(" ----- ");
+            }
+        }
+    }
+}
+    
